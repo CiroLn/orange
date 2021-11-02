07de4c9304aab02d053cb7abdd452aa0afe6f1c4
p_lnhaoli <p_lnhaoli@tencent.com>
2021-11-02 17:31:49 +0800

    layout dev

--- vitets/src/App.vue
+++ vitets/src/App.vue
@@ -1,14 +1,13 @@
 <template>
-  <el-container>
+  <el-container class="page-container">
     <el-header>Header</el-header>
     <el-container>
-<!-- 
       <el-aside width="200px">Aside</el-aside>
       <el-container>
         <el-main>Main</el-main>
         <el-footer>Footer</el-footer>
       </el-container>
-       -->
+      
     </el-container>
   </el-container>
 </template>
@@ -19,5 +18,8 @@
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;  
+}
+.page-container {
+  height: 100%;
 }
 </style>
