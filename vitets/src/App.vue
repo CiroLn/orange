6d3789c99bf145bb79c5c7e5207dbd66f3524999
p_lnhaoli <p_lnhaoli@tencent.com>
2021-11-02 17:15:27 +0800

    sider dev

--- vitets/src/App.vue
+++ vitets/src/App.vue
@@ -1,21 +1,23 @@
-<script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <el-container>
+    <el-header>Header</el-header>
+    <el-container>
+<!-- 
+      <el-aside width="200px">Aside</el-aside>
+      <el-container>
+        <el-main>Main</el-main>
+        <el-footer>Footer</el-footer>
+      </el-container>
+       -->
+    </el-container>
+  </el-container>
 </template>
 
 <style>
 #app {
+  height: 100%;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  -moz-osx-font-smoothing: grayscale;  
 }
 </style>
