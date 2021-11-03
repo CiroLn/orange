5b4e7df5ab1cf23f5298b0d3a4def2db0bbccd0c
p_lnhaoli <p_lnhaoli@tencent.com>
2021-11-03 17:24:03 +0800

    add vue-cli pro

---
+++ vitejs/src/components/MyBook1.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <h4>{{ title }}</h4>
+    <div>
+      <p>书名: {{ book.name }}</p>
+      <p>单价: {{ book.price }}</p>
+      <p>数量: {{ counter }} 元</p>
+    </div>
+  </div>
+</template>
+
+<script>
+  import { toRefs, ref, reactive } from "@vue/reactivity";
+
+  export default {
+    props: {
+      title: {
+        type: String,
+        default: 'Composition',
+      },
+    },
+    setup(props, context) {
+      // console.log(props.title);
+      // const { title } = toRefs(props);
+      // console.log('11', title.value);
+      // console.log('context: ', context);
+
+      const counter = ref(100);
+      const book = reactive({'name': 'Vue3 Guide', 'price': 66});
+      console.log('counter: ', counter.value);
+      console.log('book: ', book);
+
+      return {
+        counter,
+        book
+      }
+    }    
+  }
+</script>
